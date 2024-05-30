4b0960532458622babe05d499ef78daa5f9ede8b
ankitverma242001@gmail.com <ankitverma242001@gmail.com>
2024-05-30 14:19:36 +0530

    new commit 5

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,22 +1,28 @@
-import { User } from "../models/userSchema.js";
+
 import { catchAsyncErrors } from "./catchAsyncError.js";
 import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  const { token } = req.cookies;
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return next(new ErrorHandler("User Not Authorized", 401));
+  }
+
+  const token = authHeader.split(" ")[1]; // Extract the token from the 'Bearer <token>' format
+
   if (!token) {
     return next(new ErrorHandler("User Not Authorized", 401));
   }
+
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
   if (!decoded) {
-    throw new Error('Invalid/Expired Token, please login again')
+    throw new Error("Invalid/Expired Token, please login again");
   } else {
-    //save the user into req obj
+    // Save the user into req obj
     req.user = decoded?.id;
     next();
   }
-
-  next();
 });
